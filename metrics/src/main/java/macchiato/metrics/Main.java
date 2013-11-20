7fd59d20bc569c3555b5b25edae767e9cc86ed7e
Ciro Cavani <ciro.cavani@gmail.com>
2013-11-20 02:14:24 -0200

    empty Codahale Metrics sample

---
+++ metrics/src/main/java/macchiato/metrics/Main.java
@@ -0,0 +1,20 @@
+package macchiato.metrics;
+
+public final class Main {
+
+	private Main() {
+	}
+
+	public static void main(final String... args) throws Exception {
+		System.out.println("Macchiato Metrics start...");
+
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+
+			@Override
+			public void run() {
+				System.out.println("Macchiato Metrics shutdown...");
+			}
+
+		});
+	}
+}
