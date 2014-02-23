5b0d86de2dca354e676062372d9316eb05591958
Ciro Cavani <ciro.cavani@gmail.com>
2014-02-22 21:07:08 -0300

    Finagle: routing example

---
+++ finagle/src/main/scala/macchiato/finagle/HttpServerRoute.scala
@@ -0,0 +1,59 @@
+package macchiato.finagle
+
+import java.net.InetSocketAddress
+import java.util.Random
+
+import org.jboss.netty.buffer.ChannelBuffers
+import org.jboss.netty.handler.codec.http.HttpResponseStatus
+import org.jboss.netty.util.CharsetUtil
+
+import com.twitter.finagle.Service
+import com.twitter.finagle.builder.ServerBuilder
+import com.twitter.finagle.http.Http
+import com.twitter.finagle.http.Request
+import com.twitter.finagle.http.Response
+import com.twitter.finagle.http.RichHttp
+import com.twitter.finagle.http.service.NullService
+import com.twitter.finagle.http.service.RoutingService
+import com.twitter.util.Future
+
+object HttpServerRoute extends App {
+
+  println("Macchiato Finagle HTTP Server Route start...")
+
+  val healthcheck = new Service[Request, Response] {
+    def apply(req: Request): Future[Response] = {
+      val response = Response.apply(HttpResponseStatus.OK)
+      response.setContent(ChannelBuffers.copiedBuffer("WORKING", CharsetUtil.UTF_8))
+      Future.value(response)
+    }
+  }
+
+  val rnd = new Random();
+
+  val rand = new Service[Request, Response] {
+    def apply(req: Request): Future[Response] = {
+      val value = s"${rnd.nextLong()}";
+      val response = Response.apply(HttpResponseStatus.OK)
+      response.setContent(ChannelBuffers.copiedBuffer(value, CharsetUtil.UTF_8))
+      Future.value(response)
+    }
+  }
+
+  val service = RoutingService.byPath {
+    case "/" => NullService
+    case "/healthcheck" => healthcheck
+    case "/number" => rand
+  }
+
+  val server = ServerBuilder()
+    .codec(RichHttp[Request](Http()))
+    .bindTo(new InetSocketAddress("127.0.0.1", 8080))
+    .name("Macchiato")
+    .build(service)
+
+  sys.ShutdownHookThread {
+    println("Macchiato Finagle HTTP Server Route shutdown...")
+  }
+
+}
--- finagle/src/main/scala/macchiato/finagle/HttpServerSimple.scala
+++ finagle/src/main/scala/macchiato/finagle/HttpServerSimple.scala
@@ -16,27 +16,25 @@ import com.twitter.finagle.Service
 import com.twitter.util.Await
 import com.twitter.util.Future
 
-object HttpServerSimple {
+object HttpServerSimple extends App {
 
-  def main(args: Array[String]) {
-    println("Macchiato Finagle HTTP Server start...")
+  println("Macchiato Finagle HTTP Server start...")
 
-    val service = new Service[HttpRequest, HttpResponse] {
-      def apply(req: HttpRequest): Future[HttpResponse] = {
-        val response = new DefaultHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK)
-        response.setContent(ChannelBuffers.copiedBuffer("WORKING", CharsetUtil.UTF_8))
-        response.headers().set(HttpHeaders.Names.CONTENT_TYPE, "text/plain; charset=utf-8")
-        response.headers().set(HttpHeaders.Names.CONTENT_LENGTH, response.getContent().readableBytes())
-        Future.value(response)
-      }
+  val service = new Service[HttpRequest, HttpResponse] {
+    def apply(req: HttpRequest): Future[HttpResponse] = {
+      val response = new DefaultHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK)
+      response.setContent(ChannelBuffers.copiedBuffer("WORKING", CharsetUtil.UTF_8))
+      response.headers().set(HttpHeaders.Names.CONTENT_TYPE, "text/plain; charset=utf-8")
+      response.headers().set(HttpHeaders.Names.CONTENT_LENGTH, response.getContent().readableBytes())
+      Future.value(response)
     }
+  }
 
-    val server = Http.serve(new InetSocketAddress("127.0.0.1", 8080), service)
-    Await.ready(server)
+  val server = Http.serve(new InetSocketAddress("127.0.0.1", 8080), service)
+  Await.ready(server)
 
-    sys.ShutdownHookThread {
-      println("Macchiato Finagle HTTP Server shutdown...")
-    }
+  sys.ShutdownHookThread {
+    println("Macchiato Finagle HTTP Server shutdown...")
   }
 
 }
