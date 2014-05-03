27939bfeed8ebafa8d1af218bf564aba143d076e
Ciro Cavani <ciro.cavani@gmail.com>
2014-05-03 17:35:52 -0300

    Finagle async service

--- finagle/src/main/scala/macchiato/finagle/HttpServerSimple.scala
+++ finagle/src/main/scala/macchiato/finagle/HttpServerSimple.scala
@@ -1,6 +1,6 @@
 package macchiato.finagle
 
-import java.net.InetSocketAddress
+import java.util.concurrent.Executors
 
 import org.jboss.netty.buffer.ChannelBuffers
 import org.jboss.netty.handler.codec.http.DefaultHttpResponse
@@ -9,28 +9,35 @@ import org.jboss.netty.handler.codec.http.HttpRequest
 import org.jboss.netty.handler.codec.http.HttpResponse
 import org.jboss.netty.handler.codec.http.HttpResponseStatus
 import org.jboss.netty.handler.codec.http.HttpVersion
-import org.jboss.netty.util.CharsetUtil
 
 import com.twitter.finagle.Http
 import com.twitter.finagle.Service
+import com.twitter.io.Charsets
 import com.twitter.util.Await
 import com.twitter.util.Future
+import com.twitter.util.FuturePool
 
 object HttpServerSimple extends App {
 
   println("Macchiato Finagle HTTP Server start...")
 
+  val executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors())
+  val async = FuturePool(executor)
+
+  val RESPONSE_RAW = ChannelBuffers.wrappedBuffer("WORKING".getBytes(Charsets.Utf8))
+  val RESPONSE = ChannelBuffers.unmodifiableBuffer(RESPONSE_RAW);
+
   val service = new Service[HttpRequest, HttpResponse] {
-    def apply(req: HttpRequest): Future[HttpResponse] = {
+    def apply(req: HttpRequest): Future[HttpResponse] = async {
       val response = new DefaultHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK)
-      response.setContent(ChannelBuffers.copiedBuffer("WORKING", CharsetUtil.UTF_8))
+      response.setContent(RESPONSE)
       response.headers().set(HttpHeaders.Names.CONTENT_TYPE, "text/plain; charset=utf-8")
       response.headers().set(HttpHeaders.Names.CONTENT_LENGTH, response.getContent().readableBytes())
-      Future.value(response)
+      response
     }
   }
 
-  val server = Http.serve(new InetSocketAddress("127.0.0.1", 8080), service)
+  val server = Http.serve("127.0.0.1:8080", service)
   Await.ready(server)
 
   sys.ShutdownHookThread {
