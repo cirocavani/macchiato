60e1c5d4d636f23568eb04afea816ad60e3c777e
Ciro Cavani <ciro.cavani@gmail.com>
2014-05-03 21:32:36 -0300

    Dagger: simple example with gradle/eclipse integration

--- dagger/src/main/java/macchiato/dagger/Main.java
+++ dagger/src/main/java/macchiato/dagger/Main.java
@@ -1,12 +1,52 @@
 package macchiato.dagger;
 
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.ObjectGraph;
+import dagger.Provides;
+
 public final class Main {
 
 	private Main() {
 	}
 
+	static class Config {
+
+		void dump() {
+			System.out.println("x=y");
+		}
+	}
+
+	static class App {
+
+		@Inject
+		Config config;
+
+		public void start() {
+			config.dump();
+		}
+
+	}
+
+	@Module(injects = App.class)
+	static class MainModule {
+
+		@Provides
+		@Singleton
+		Config provideConfig() {
+			return new Config();
+		}
+
+	}
+
 	public static void main(final String... args) throws Exception {
 		System.out.println("Macchiato Dagger start...");
+
+		final ObjectGraph objectGraph = ObjectGraph.create(new MainModule());
+		final App app = objectGraph.get(App.class);
+		app.start();
 
 		Runtime.getRuntime().addShutdownHook(new Thread() {
 
